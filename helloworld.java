c21fac6c8444bee6c479ba2cdf6e920452cc76d2
Kyathisri-04 <2100031097@kluniversity.in>
2024-01-11 09:54:59 +0530

    simple java program

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+class main{
+public static void main(String[args])[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A
+[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D{
+Syste,out.println("Hello World");
+}
+}[A[A[C[C[C[Cm.out.println("He[A[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[B[B[B[B[B[D
+cat helloworld.java
